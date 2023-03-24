ad9ef1fdd07953c8c790ab89d2a10a9e0822fa8d
a676-code <109701008+thisusernameisaparadox@users.noreply.github.com>
2023-03-24 19:27:32 -0400

    added plots

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # prime-constant
 A short script that computes the constant discussed in [this video](https://www.youtube.com/watch?v=_gCKX6VMvmU). 
+
+## Plotting the Average as $n \to \infty$
+Scatterplot of the first 14 averages:
+![14 averages](averages_14.png)
+Scatterplot of the first 194 averages:
+![194 averages](averages_194.png)
+Scatterplot of the first 1,994 averages:
+![1994 averages](averages_1994.png)
+Scatterplot of the first 19,994 averages:
+![19,994 averages](averages_19994.png)
+Scatterplot of the first 199,994 averages:
+![199,994 averages](averages_199994.png)
--- prime_constant.py
+++ prime_constant.py
@@ -3,6 +3,9 @@
 # 19/3/23
 # Purpose: Compute the constant discussed in this (https://www.youtube.com/watch?v=_gCKX6VMvmU) video
 import math
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 def prime(n):
     if n == 1:
@@ -13,17 +16,123 @@ def prime(n):
             return False
     return True
 
-sequence = []
-for n in range(1, 100000000):
-    # loop through the numbers less than n
-    for i in range(2, 1000000):
-        # if prime and doesn't divide n, add it to the sequence and move to the next number 
-        if prime(i) and n % i != 0:
-            sequence.append(i)
-            break
-total = 0
-for x in sequence:
-    total += x
+def generate(n):
+    sequence = []
+    while len(sequence) < n:
+        for m in range(1, n):
+            # loop through the numbers less than m
+            for i in range(2, m):
+                # if prime and doesn't divide m, add it to the sequence and move to the next number 
+                if prime(i) and m % i != 0 and len(sequence) < n:
+                    sequence.append(i)
+                    break
+    return sequence
 
-average = total / len(sequence)
-print("average:", average)
+sequence = generate(20)
+print(sequence)
+
+# Plots of Averages
+n = 10
+sequence = generate(n)
+
+average_sequence = []
+total = 0
+for i, s in enumerate(sequence):
+    total += s
+    average = total / (i + 1)
+    average_sequence.append(average)
+print(len(average_sequence))
+
+average_df = pd.DataFrame(average_sequence, columns=["Average"])
+
+indices = []
+for i in range(len(average_sequence)):
+    indices.append(i)
+average_df['index'] = indices
+
+sns.scatterplot(x="index", y="Average", data=average_df)
+plt.show()
+
+n = 100
+sequence = generate(n)
+
+average_sequence = []
+total = 0
+for i, s in enumerate(sequence):
+    total += s
+    average = total / (i + 1)
+    average_sequence.append(average)
+print(len(average_sequence))
+
+average_df = pd.DataFrame(average_sequence, columns=["Average"])
+
+indices = []
+for i in range(len(average_sequence)):
+    indices.append(i)
+average_df['index'] = indices
+
+sns.scatterplot(x="index", y="Average", data=average_df)
+plt.show()
+
+n = 1000
+sequence = generate(n)
+
+average_sequence = []
+total = 0
+for i, s in enumerate(sequence):
+    total += s
+    average = total / (i + 1)
+    average_sequence.append(average)
+print(len(average_sequence))
+
+average_df = pd.DataFrame(average_sequence, columns=["Average"])
+
+indices = []
+for i in range(len(average_sequence)):
+    indices.append(i)
+average_df['index'] = indices
+
+sns.scatterplot(x="index", y="Average", data=average_df)
+plt.show()
+
+n = 10000
+sequence = generate(n)
+
+average_sequence = []
+total = 0
+for i, s in enumerate(sequence):
+    total += s
+    average = total / (i + 1)
+    average_sequence.append(average)
+print(len(average_sequence))
+
+average_df = pd.DataFrame(average_sequence, columns=["Average"])
+
+indices = []
+for i in range(len(average_sequence)):
+    indices.append(i)
+average_df['index'] = indices
+
+sns.scatterplot(x="index", y="Average", data=average_df)
+plt.show()
+
+n = 100000
+sequence = generate(n)
+
+average_sequence = []
+total = 0
+for i, s in enumerate(sequence):
+    total += s
+    average = total / (i + 1)
+    average_sequence.append(average)
+print(len(average_sequence))
+
+average_df = pd.DataFrame(average_sequence, columns=["Average"])
+
+indices = []
+for i in range(len(average_sequence)):
+    indices.append(i)
+average_df['index'] = indices
+
+sns.scatterplot(x="index", y="Average", data=average_df)
+plt.show()
